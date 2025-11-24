33bae9790384a54e0615d4f6a8c4a47b4edb48f6
Max <notamonkeirl@gmail.com>
2025-11-24 21:12:22 +0100

    Add files via upload

---
+++ MultipleRobloxInstances/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace MultipleRobloxInstances
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex(true, "ROBLOX_singletonMutex");
+
+            if (!mutex.WaitOne(TimeSpan.Zero, false))
+            {
+                Console.WriteLine("Made by NotAMonkeIRL");
+                Console.WriteLine("Close your Roblox");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Made by NotAMonkeIRL");
+            Console.WriteLine("You can open Roblox now.");
+            Console.WriteLine("Keep this open while having your Roblox instances open. Press a key to exit.");
+            Console.ReadKey();
+            Console.WriteLine("Are you sure that you want to close all of your Instances?");
+            Console.ReadKey();
+            mutex.ReleaseMutex();
+            mutex.Dispose();
+        }
+    }
+}
